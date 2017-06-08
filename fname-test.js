a57b0c546061639a7e40027cf9badc6ac963014c
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-08 11:41:05 +0800

    Updated test.

--- fname-test.js
+++ fname-test.js
@@ -2,19 +2,25 @@
 const assert = require( "assert" );
 const fname = require( "./fname.js" );
 
-assert.equal( fname( function test( ){ } ), "test", "should return function name 'test'" );
-
-assert.equal( fname( function( ){ } ), "", "should return empty function name" );
+assert.equal( fname( ), "", "should return empty string" );
 
 assert.equal( fname( "test" ), "test", "should return string 'test'" );
 
 assert.equal( fname( "123" ), "", "should return empty string" );
 
-assert.equal( fname( ), "", "should return empty string" );
+assert.equal( fname( 123 ), "", "should return empty string" );
 
 assert.equal( fname( false ), "", "should return empty string" );
 
+assert.equal( fname( { "name": "yeah" } ), "", "should return empty string" );
+
 assert.equal( fname( NaN ), "", "should return empty string" );
+
+assert.equal( fname( Array ), "Array", "should return 'Array'" );
+
+assert.equal( fname( function( ){ } ), "", "should return empty function name" );
+
+assert.equal( fname( function test( ){ } ), "test", "should return function name 'test'" );
 
 assert.equal( fname( ( { test( ){ } } ).test ), "test", "should return function name 'test'" )
 
--- fname.js
+++ fname.js
@@ -35,6 +35,9 @@
 			"module": "fname",
 			"author": "Richeve S. Bebedor",
 			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+			],
 			"repository": "https://github.com/volkovasystems/fname.git",
 			"test": "fname-test.js",
 			"global": true
