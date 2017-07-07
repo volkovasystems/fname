8238aef61a3cafebc33afe56f74e710b117288f2
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-07 14:00:06 +0800

    Updated server/client test.

--- fname-test.js
+++ fname-test.js
@@ -2,26 +2,20 @@
 const assert = require( "assert" );
 const fname = require( "./fname.js" );
 
-assert.equal( fname( ), "", "should return empty string" );
-
 assert.equal( fname( "test" ), "test", "should return string 'test'" );
-
-assert.equal( fname( "123" ), "", "should return empty string" );
-
-assert.equal( fname( 123 ), "", "should return empty string" );
-
-assert.equal( fname( false ), "", "should return empty string" );
-
-assert.equal( fname( { "name": "yeah" } ), "", "should return empty string" );
-
-assert.equal( fname( NaN ), "", "should return empty string" );
-
-assert.equal( fname( Array ), "Array", "should return 'Array'" );
-
-assert.equal( fname( function( ){ } ), "", "should return empty function name" );
 
 assert.equal( fname( function test( ){ } ), "test", "should return function name 'test'" );
 
 assert.equal( fname( ( { test( ){ } } ).test ), "test", "should return function name 'test'" )
+
+assert.equal( fname( Error ), "Error", "should return 'Error'" );
+
+assert.equal( fname( ), "", "should return empty string" );
+
+assert.equal( fname( function( ){ } ), "", "should return empty string" );
+
+assert.equal( fname( ( ) => { } ), "", "should return empty string" );
+
+assert.equal( fname( ( entity ) => ( typeof entity == "string" ) ), "", "should return empty string" );
 
 console.log( "ok" );
